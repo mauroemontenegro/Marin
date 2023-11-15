6114c77aafcaefcfd878e5e5248e7838f7ce285a
mauroemontenegro <mmontenegro405@gmail.com>
2023-11-14 21:58:45 -0300

    proyectofinal2

--- src/components/ItemListContainer/itemListContainer.jsx
+++ src/components/ItemListContainer/itemListContainer.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import styles from './list.module.css'
 import Categories from "../Categorias/Categories";
 import ItemList from "../ItemList/ItemList";
+import inicio from "../../imagenes/inico2.webp"
 
 
 const ItemListContainer = ({ greting }) => {
@@ -9,6 +10,7 @@ const ItemListContainer = ({ greting }) => {
 
     return (
         <>
+        <img className="img-inicio" src={inicio} alt="" />
             <h3 className="titulo-prod">PRODUCTOS DESTACADOS</h3>
             <div className="categorias">
                 <h3>CATEGORIAS:</h3>
